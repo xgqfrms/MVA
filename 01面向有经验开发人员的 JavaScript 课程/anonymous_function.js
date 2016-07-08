47f9c325dceef99e46520c88c6fb5cfb540d64a0
xgqfrms <xgqfrms@outlook.com>
2016-07-08 21:03:55 +0800

    ok

--- 01面向有经验开发人员的 JavaScript 课程/anonymous_function.js
+++ 01面向有经验开发人员的 JavaScript 课程/anonymous_function.js
@@ -1,3 +1,8 @@
+/*
+Application Scenarios(应用场景):
+enclosures && recursion (闭包，递归)
+*/
+
 // anonymous function
 var x = "global var"; 
 function anoy(name){
@@ -10,9 +15,56 @@ anoy("xgqfrms");
 
 
 function myTime() {
-    alert("just do it!");
+    // alert("just do it!");
+    console.log("just do it!");
 }
+//function 能在任何地方(fun声明：前面/后面)，调用
 
-var myTime = function(){
-    alert("anonymous:  just do it!");
+var myTime_anoy = function(){
+    // alert("anonymous:  just do it!");
+    console.log("anonymous:  just do it!");
 }
+// var 只有在var被初始化后，才能使用
+
+myTime();
+console.log(myTime_anoy);
+
+
+
+// function expression
+var sayHi = function(){
+    // alert("Hi, function expression!");
+    console.log("Hi, function expression!");
+};
+console.log(sayHi);
+
+//in a object
+var xray = {
+    sayName: function(){
+        // alert("Hi, My name is xray!");
+        console.log("Hi, My name is xray!");
+    }
+};
+console.log(xray);
+
+// event handler
+/*
+$("p").click = function(){
+    alert("Hi, JQuery!");
+    console.log("Hi, JQuery!");
+}
+*/
+
+//self invoking function(自调用)
+((function(){
+    // alert("Hi, self invoking function!");
+    console.log("Hi, self invoking function!");
+})());
+
+
+//factorial(阶乘)
+var factorial = function(n){
+    return !(n>1) ? 1 : arguments.callee(n-1)*n;//callee (被调用函数)
+}
+console.log(factorial(10));
+
