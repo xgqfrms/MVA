052fff1cd29c0202fb0cc42fdd5f8d10f64327a4
xgqfrms <xgqfrms@outlook.com>
2016-07-09 15:24:45 +0800

    oop

--- 01面向有经验开发人员的 JavaScript 课程/OOP.js
+++ 01面向有经验开发人员的 JavaScript 课程/OOP.js
@@ -1,12 +1,38 @@
 // 
 
-var myGrades = {};
+var myGrades = {};//empty object
 
+var collegeAlgebra = {//way 1 x = {A : B},same as JSON
+    level : "freshman",
+    difficulty : "hard",
+    expectedGrade : 98,
 
+    textbook : function(){
+        console.log("College Algebra for freshman is the required text!");
+    }
+}
 
+var collegeAlg = new Object;//way 2, x = new Object; x.Attr = value
 
+collegeAlg.level = "freshman";
+collegeAlg.difficulty = "hard",
+collegeAlg.expectedGrade = 98,
+collegeAlg.textbook = function(){
+        console.log("College Algebra for freshman is the required text!");
+    }
 
+console.log(collegeAlgebra);
+console.log("\n");
+console.log("xgqfrms is a " + collegeAlgebra.level);
+// alert("xgqfrms is a " + collegeAlgebra.level + "!");
+//node.js not support alert function!
+console.log("\n");
+collegeAlgebra.textbook();
 
+console.log("\n");
+console.log(collegeAlg);
+console.log("\n");
+collegeAlg.textbook();
 
 
 
