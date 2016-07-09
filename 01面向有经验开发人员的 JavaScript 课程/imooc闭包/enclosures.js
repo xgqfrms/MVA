adb2e57c32f08892480055fd679d14550f08bdbd
xgqfrms <xgqfrms@outlook.com>
2016-07-09 12:20:01 +0800

    ok

--- 01面向有经验开发人员的 JavaScript 课程/imooc闭包/enclosures.js
+++ 01面向有经验开发人员的 JavaScript 课程/imooc闭包/enclosures.js
@@ -1,11 +1,31 @@
-// 
+//
+function test(){
+
 document.body.innerHTML = 
 "<div id='div1'>111</div>" + "<div id='div2'>222</div>" +
 "<div id='div3'>333</div>" + "<div id='div4'>444</div>";
 
 for (var i = 0; i < 4; i++) {
     document.getElementById('div'+i).addEventListener('click',function(){
-        alert(i);
+        alert(i);//4
     });
 }
 
+};
+test();
+
+function newTest() {
+document.body.innerHTML = 
+"<div id='div1'>111</div>" + "<div id='div2'>222</div>" +
+"<div id='div3'>333</div>" + "<div id='div4'>444</div>";
+
+for (var i = 0; i < 4; i++) {
+    !function(i){
+        document.getElementById('div'+i).addEventListener('click',function(){
+        alert(i);//1,2,3
+     });
+    }(i);
+  }
+}
+
+newTest();
