70e4a8be7cac815e783c2c6fcb554c78f440c495
xgqfrms <xgqfrms@outlook.com>
2016-07-09 18:10:33 +0800

    ok

--- 01面向有经验开发人员的 JavaScript 课程/OOP.js
+++ 01面向有经验开发人员的 JavaScript 课程/OOP.js
@@ -3,36 +3,52 @@
 var myGrades = {};//empty object
 
 var collegeAlgebra = {//way 1 x = {A : B},same as JSON
-    level : "freshman",
-    difficulty : "hard",
-    expectedGrade : 98,
+    level: "freshman",
+    difficulty: "hard",
+    expectedGrade: 98,
 
-    textbook : function(){
+    textbook: function () {
         console.log("College Algebra for freshman is the required text!");
     }
 }
 
 var collegeAlg = new Object;//way 2, x = new Object; x.Attr = value
-
-collegeAlg.level = "freshman";
-collegeAlg.difficulty = "hard",
-collegeAlg.expectedGrade = 98,
-collegeAlg.textbook = function(){
+    collegeAlg.level = "freshman",
+    collegeAlg.difficulty = "hard",
+    collegeAlg.expectedGrade = 98,
+    collegeAlg.textbook = function () {
         console.log("College Algebra for freshman is the required text!");
+    },
+    collegeAlg["textBook"] = function () {
+        console.log("College Algebra  textBook!");
     }
 
-console.log(collegeAlgebra);
-console.log("\n");
-console.log("xgqfrms is a " + collegeAlgebra.level);
+console.log("xgqfrms is a " + collegeAlgebra.level + "!");
 // alert("xgqfrms is a " + collegeAlgebra.level + "!");
-//node.js not support alert function!
-console.log("\n");
+// node.js not support alert function!
+
+console.log("\nDemo 1:");
+console.log(collegeAlgebra);
 collegeAlgebra.textbook();
 
-console.log("\n");
+
+console.log("\nDemo 2:");
 console.log(collegeAlg);
-console.log("\n");
 collegeAlg.textbook();
 
 
+console.log("\nDemo 3:");
+console.log(collegeAlg);
+collegeAlg["textBook"]();
+collegeAlg.textBook();
 
+console.log("\t"+collegeAlg["textBook"]());//output + undefined ?
+console.log("\t"+collegeAlg.textBook());//output + undefined ?
+
+
+console.log("\nDemo abc+xyz:");
+var abc = collegeAlg["textBook"]();//output
+var xyz = collegeAlg.textBook();//output
+
+console.log("\t"+abc);// undefined
+console.log("\t"+xyz);// undefined
